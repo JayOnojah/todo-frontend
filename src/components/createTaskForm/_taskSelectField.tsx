bc968f060338b9c831b2bb6896f06eeb95c77c72
Jeremiah Onojah <jayonojah@gmail.com>
2024-01-12 15:05:16 +0100

    Completion of UI

--- src/components/createTaskForm/_taskSelectField.tsx
+++ src/components/createTaskForm/_taskSelectField.tsx
@@ -4,24 +4,51 @@ import {
   InputLabel,
   MenuItem,
   Select,
-  //SelectChangeEvent,
+  SelectChangeEvent,
 } from '@mui/material';
+import { ISelectField } from './interfaces/ISelectField';
+import PropTypes from 'prop-types';
 
-export const TaskSelectField: FC = (): ReactElement => {
+export const TaskSelectField: FC<ISelectField> = (props): ReactElement => {
+  const {
+    value = '',
+    label = 'Select Box',
+    name = 'selectBox',
+    items = [{ value: '', label: 'Add Items' }],
+    disabled = false,
+    onChange = (e: SelectChangeEvent) => console.log(e),
+  } = props;
   return (
     <FormControl fullWidth size="small">
-      <InputLabel id="status">Status</InputLabel>
+      <InputLabel id={`${name}-id`}>{label}</InputLabel>
       <Select
-        labelId="status"
-        id="status-select"
-        value=""
-        label="Status"
-        name="status"
+        labelId={`${name}-id`}
+        id={`${name}-id-select`}
+        value={value}
+        label={label}
+        name={name}
+        onChange={onChange}
+        disabled={disabled}
       >
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem>
+        {items.map((item, index) => (
+          <MenuItem key={item.value + index} value={item.value}>
+            {item.label}
+          </MenuItem>
+        ))}
       </Select>
     </FormControl>
   );
+};
+
+TaskSelectField.propTypes = {
+  onChange: PropTypes.func,
+  label: PropTypes.string,
+  name: PropTypes.string,
+  disabled: PropTypes.bool,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+    }).isRequired,
+  ),
 };
